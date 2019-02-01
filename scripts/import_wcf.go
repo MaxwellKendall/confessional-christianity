00b5c6460a90cb778d9b997a69274729456390f0
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-02-01 08:33:55 -0500

    adding new script for .txt files

--- scripts/import_wcf.go
+++ scripts/import_wcf.go
@@ -3,11 +3,14 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"go/scanner"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
 
-func main() {
+func readCSV() {
 	// gives me a pointer to a File which implements the Reader Interface
 	fileToBeRead, err := os.Open("./WCF.csv")
 	if err != nil {
@@ -23,4 +26,17 @@ func main() {
 
 	fmt.Println(records[0][1])
 	fmt.Println("HERE")
+}
+
+func main() {
+	readCSV()
+
+	const src, _ = ioutil.ReadFile("./WCF.txt")
+
+	var s scanner.Scanner
+	s.Init(strings.NewReader(src))
+	s.Filename = "example"
+	for tok := s.Scan(); tok != scanner.EOF; tok = s.Scan() {
+		fmt.Printf("%s: %s\n", s.Position, s.TokenText())
+	}
 }
