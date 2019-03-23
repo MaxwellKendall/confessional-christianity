1e42b16fc103b50fb35562c688b79de77dca886f
Maxwell Kendall <maxwellkendall@Maxwells-MacBook-Pro.local>
2019-03-23 11:51:09 -0400

    successfully reading from and posting to the db

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -12,30 +13,61 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
-// Chapter comment
-type Chapter struct {
+// WcfQuery comment
+type WcfQuery struct {
 	ID      string `json:"id"`
-	Whateva string `json:"whateva"`
+	Chapter int    `json:"chapter"`
+}
+
+func getDBSession() *dynamodb.DynamoDB {
+	// lets read something from dynamo db
+	sess := session.Must(session.NewSession())
+	return dynamodb.New(sess)
+}
+
+func listTables(svc *dynamodb.DynamoDB) {
+	result, err := svc.ListTables(&dynamodb.ListTablesInput{})
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Tables:")
+	fmt.Println("")
+
+	for _, n := range result.TableNames {
+		fmt.Println(*n)
+	}
+}
+
+func get(svc *dynamodb.DynamoDB, query map[string]*dynamodb.AttributeValue) (*dynamodb.GetItemOutput, error) {
+	return svc.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String("wcf"),
+		Key:       query,
+	})
+}
+
+func update(svc *dynamodb.DynamoDB, query map[string]*dynamodb.AttributeValue) (*dynamodb.PutItemOutput, error) {
+	return svc.PutItem(&dynamodb.PutItemInput{
+		TableName: aws.String("wcf"),
+		Item:      query,
+	})
 }
 
 func main() {
-	// lets read something from dynamo db
-	sess := session.Must(session.NewSession())
-	svc := dynamodb.New(sess)
-	test := Chapter{
+	svc := getDBSession()
+	listTables(svc)
+
+	// Read from DB
+	chapter1 := WcfQuery{
 		ID:      "1",
-		Whateva: "we",
-	}
-	key, err := dynamodbattribute.MarshalMap(test)
-	if err != nil {
-		fmt.Println("YIZO")
-	}
-	input := &dynamodb.GetItemInput{
-		Key:       key,
-		TableName: aws.String("test2"),
+		Chapter: int(1),
 	}
 
-	result, err := svc.GetItem(input)
+	chapter1Query, err := dynamodbattribute.MarshalMap(chapter1)
+	result, err := get(svc, chapter1Query)
+
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
 			switch aerr.Code() {
@@ -58,6 +90,5 @@ func main() {
 		return
 	}
 
-	fmt.Println(result)
-
+	fmt.Println("here it is bra", result.Item)
 }
